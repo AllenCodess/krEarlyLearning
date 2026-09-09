3b941f52b54df155e13f5eade207c67824bf4f26
AllenCodess <Aph296@protonmail.com>
2026-09-08 20:58:06 -0400

    started the layout of the hero section

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,19 @@
 export const HomePage = () => {
   return (
     <>
-      <h1>HomePage</h1>
+      <div className="hero-section container">
+        <div className="hero-section-text-container">
+          <h1 className="hero-header">Building Bright Minds & Strong Futures</h1>
+          <p className="hero-text">
+            A safe, nuturing, and enriching home-based learning environment where childeren learn,
+            grow, and thrive.
+          </p>
+          <button className="hero-btn">Schedule A tour</button>
+        </div>
+        <div className="hero-img-container">
+          <img className="hero-img" src="/frontend/public/images/heroimg.png" alt="" />
+        </div>
+      </div>
     </>
   );
 };
