c9e9e1c9de4ed06010f6e1fa64042192db4afeea
AllenCodess <Aph296@protonmail.com>
2026-09-09 11:44:35 -0400

    added the desktop version of the why choose up section

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import {
+  faHeart,
+  faArrowRight,
+  faCheckCircle,
+  faAppleWhole,
+  faSun,
+  faStar,
+  faBook,
+  faLeaf,
+  faClock,
+} from "@fortawesome/free-solid-svg-icons";
 
 export const HomePage = () => {
   return (
@@ -25,6 +35,85 @@ export const HomePage = () => {
             </li>
             <li className="hero-list-item green">THRIVE</li>
           </ul>
+        </div>
+      </div>
+      <div className="why-choose-us-section container">
+        <div className="why-choose-text-container">
+          <h3 className="why-choose-sub-header green">WHY CHOOSE US</h3>
+          <h2 className="why-choose-main-header">A Home Where Children Thrive</h2>
+          <p className="why-choose-description">
+            Every child deserves a safe, nuturing environment where they can grow, learn, and
+            explore at thier own pace.
+          </p>
+          <div className="why-choose-grid-container">
+            <div className="why-choose-grid-item">
+              <FontAwesomeIcon className="green fonticonwhy" icon={faHeart} />
+
+              <h4 className="item-heading">Welcome Home Environment</h4>
+              <p className="item-desc grey">
+                A warm, home-like setting where your child feels safe, comfortable, and loved
+                everyday.
+              </p>
+            </div>
+            <div className="why-choose-grid-item">
+              <FontAwesomeIcon className="green fonticonwhy" icon={faCheckCircle} />
+
+              <h4 className="item-heading">Licensed & Certified</h4>
+              <p className="item-desc grey">
+                State of Maryland licensed family child care with over 200 hours of professional
+                training.
+              </p>
+            </div>
+            <div className="why-choose-grid-item">
+              <FontAwesomeIcon className="green fonticonwhy" icon={faAppleWhole} />
+
+              <h4 className="item-heading">Nutritious Meals Provided</h4>
+              <p className="item-desc grey">
+                Healthy breakfast, lunch and snacks prepared fresh daily for all children.
+              </p>
+            </div>
+            <div className="why-choose-grid-item">
+              <FontAwesomeIcon className="green fonticonwhy" icon={faSun} />
+
+              <h4 className="item-heading">Daily Outdoor Play</h4>
+              <p className="item-desc grey">
+                Supervised outdoor activities that encourage physical development and exploration.
+              </p>
+            </div>
+            <div className="why-choose-grid-item">
+              <FontAwesomeIcon className="green fonticonwhy" icon={faStar} />
+
+              <h4 className="item-heading">Individualized Attention</h4>
+              <p className="item-desc grey">
+                Small group size ensures each child receives personalized care and learning support.
+              </p>
+            </div>
+            <div className="why-choose-grid-item">
+              <FontAwesomeIcon className="green fonticonwhy" icon={faBook} />
+
+              <h4 className="item-heading">Daily infant Report Cards</h4>
+              <p className="item-desc grey">
+                Parents of infants receive detailed daily reports on feeding and naps.
+              </p>
+            </div>
+            <div className="why-choose-grid-item">
+              <FontAwesomeIcon className="green fonticonwhy" icon={faLeaf} />
+
+              <h4 className="item-heading">Learning Through Play</h4>
+              <p className="item-desc grey">
+                Age-appropriate activities including crafts, circle time, music, sensory
+                exploration, and story time.
+              </p>
+            </div>
+            <div className="why-choose-grid-item">
+              <FontAwesomeIcon className="green fonticonwhy" icon={faClock} />
+
+              <h4 className="item-heading">Flexible Hours</h4>
+              <p className="item-desc grey">
+                Open Monday through Friday, 7:30AM to 5:30PM to fit working families' schedules.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
