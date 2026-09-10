0967512345de2acef08cc7001eb2de03ad7cfe74
AllenCodess <Aph296@protonmail.com>
2026-09-10 19:52:30 -0400

    started the layout of the programs page

--- frontend/src/pages/ProgramsPage.jsx
+++ frontend/src/pages/ProgramsPage.jsx
@@ -1,7 +1,17 @@
 export const ProgramPage = () => {
   return (
     <>
-      <h1>ProgramPage</h1>
+      <div className="pre-program-section container">
+        <div className="program-header">
+          <h1 className="programheader green">Our Programs</h1>
+          <p className="subheaderone">Nuturing every stage. Supporting every milestone.</p>
+          <p className="subheaderotwo">
+            At KR Early Learning Academy, we provide a safe, loving, and enriching environment where
+            children learn, grow, and thrive. Each room is thoughtfully designed to meet the
+            developmental needs of the children it serves.
+          </p>
+        </div>
+      </div>
     </>
   );
 };
