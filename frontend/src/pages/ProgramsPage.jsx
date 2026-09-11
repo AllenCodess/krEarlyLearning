62fa1c1ec6c918ab533e7a9010846b4f97197b02
AllenCodess <Aph296@protonmail.com>
2026-09-10 20:22:09 -0400

    got the cards structure for the programs page

--- frontend/src/pages/ProgramsPage.jsx
+++ frontend/src/pages/ProgramsPage.jsx
@@ -11,6 +11,72 @@ export const ProgramPage = () => {
             developmental needs of the children it serves.
           </p>
         </div>
+        <div className="program-cards-section">
+          <div className="program-card">
+            <img
+              className="program-img"
+              src="/public/images/toddler.png"
+              alt="Toddler Program picture"
+            />
+            <div className="program-card-text-container">
+              <p className="ages-program">AGES 0-18 MONTHS</p>
+              <h3 className="program-card-header">Infant & Young Toddler Room</h3>
+              <p className="program-card-text">
+                A calm, cozy space designed for your baby's safety and comfort with soft lighting
+                and soothing colors.
+              </p>
+              <ul className="program-card-list">
+                <li className="program-list-items">
+                  Safe sleep environment following AAP guidelines
+                </li>
+                <li className="program-list-items">
+                  Safe sleep environment following AAP guidelines
+                </li>
+                <li className="program-list-items">
+                  Safe sleep environment following AAP guidelines
+                </li>
+                <li className="program-list-items">
+                  Safe sleep environment following AAP guidelines
+                </li>
+                <li className="program-list-items">
+                  Safe sleep environment following AAP guidelines
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="program-card">
+            <img
+              className="program-img"
+              src="/public/images/toddler.png"
+              alt="Toddler Program picture"
+            />
+            <div className="program-card-text-container">
+              <p className="ages-program">AGES 0-18 MONTHS</p>
+              <h3 className="program-card-header">Infant & Young Toddler Room</h3>
+              <p className="program-card-text">
+                A calm, cozy space designed for your baby's safety and comfort with soft lighting
+                and soothing colors.
+              </p>
+              <ul className="program-card-list">
+                <li className="program-list-items">
+                  Safe sleep environment following AAP guidelines
+                </li>
+                <li className="program-list-items">
+                  Safe sleep environment following AAP guidelines
+                </li>
+                <li className="program-list-items">
+                  Safe sleep environment following AAP guidelines
+                </li>
+                <li className="program-list-items">
+                  Safe sleep environment following AAP guidelines
+                </li>
+                <li className="program-list-items">
+                  Safe sleep environment following AAP guidelines
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
