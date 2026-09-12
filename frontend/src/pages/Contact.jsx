ce2f02544a8f02ff7ea9376703fd961fa4ebde9c
AllenCodess <Aph296@protonmail.com>
2026-09-12 12:34:47 -0400

    set up the contact form with useState

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,7 +1,26 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot, faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 export const Contact = () => {
+  const [formData, setFormData] = useState({
+    Name: "",
+    Email: "",
+    Phone: "",
+    Message: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
   return (
     <>
       <div className="contact-container container">
@@ -9,9 +28,9 @@ export const Contact = () => {
           <h2 className="contact-subheader green">Contact Us</h2>
           <h1 className="contact-main-header green">KR Early Learning Academy</h1>
           <p className="contact-desc">
-            We’d love to hear from you! Families can reach out with any questions, schedule a tour,
+            We'd love to hear from you! Families can reach out with any questions, schedule a tour,
             ask about enrollment, or learn more about our programs. At KR Early Learning Academy,
-            we’re committed to providing a safe, loving, and enriching environment where infants,
+            we're committed to providing a safe, loving, and enriching environment where infants,
             toddlers, and preschool children learn, grow, and thrive.
           </p>
           <div className="contact-footer-container">
@@ -35,14 +54,39 @@ export const Contact = () => {
               <h1 className="form-heading green">Send Us A Message</h1>
               <p className="form-text">We'll get back to you as soon as possible!</p>
             </div>
-            <form className="contact-form-inputs">
-              <input className="form-input" type="text" placeholder="Name" />
-              <input className="form-input" type="text" placeholder="Email" />
-              <input className="form-input" type="text" placeholder="Phone" />
-              <textarea className="form-message" placeholder="Message"></textarea>
-              <button className="form-btn" type="submit">
-                Send
-              </button>
+            <form onSubmit={handleSubmit} className="contact-form-inputs">
+              <input
+                className="form-input"
+                name="Name"
+                value={formData.Name}
+                type="text"
+                placeholder="Name"
+                onChange={handleChange}
+              />
+              <input
+                className="form-input"
+                type="text"
+                name="Email"
+                value={formData.Eamil}
+                placeholder="Email"
+                onChange={handleChange}
+              />
+              <input
+                className="form-input"
+                type="text"
+                name="Phone"
+                value={formData.Phone}
+                placeholder="Phone"
+                onChange={handleChange}
+              />
+              <textarea
+                className="form-message"
+                name="Message"
+                value={formData.Message}
+                placeholder="Message"
+                onChange={handleChange}
+              ></textarea>
+              <button className="form-btn">Send</button>
             </form>
           </div>
         </div>
