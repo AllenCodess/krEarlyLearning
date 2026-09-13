b7b3a2d05ca3958e8fc3f229db0891768b49bdd4
AllenCodess <Aph296@protonmail.com>
2026-09-13 15:19:55 -0400

    structuring the env file

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 const http = require("http");
 
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env" });
+
+const port = process.env.PORT;
+
+console.log(port);
+
 const server = http.createServer((req, res) => {
   res.end("Hello from the server");
 });
 
-server.listen(8000, () => {
-  console.log("listening on port 8000");
+server.listen(port, () => {
+  console.log(`app is running on port ${port}`);
 });
