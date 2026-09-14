512947f6d891fbc4a85cdac4681d5c16737d16d9
AllenCodess <Aph296@protonmail.com>
2026-09-13 20:38:47 -0400

    created DB and conncted to it from vs code

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,28 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 
 const dotenv = require("dotenv");
 dotenv.config({ path: ".env" });
 
+const mongoose = require("mongoose");
+const DB = process.env.DATABASE;
 const port = process.env.PORT;
 
-console.log(port);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(DB);
+    console.log("Sucessfully connected to database");
+  } catch (error) {
+    console.error("Error connecting to MongoDB", error.message);
+  }
+};
 
-const server = http.createServer((req, res) => {
-  res.end("Hello from the server");
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Hello from express");
 });
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`app is running on port ${port}`);
 });
