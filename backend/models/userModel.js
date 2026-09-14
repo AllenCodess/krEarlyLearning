4198287099c304efcf69373976971d8444dff4c0
AllenCodess <Aph296@protonmail.com>
2026-09-14 10:21:37 -0400

    created user schema

---
+++ backend/models/userModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, "A user must have a name."],
+    trim: true,
+  },
+  email: {
+    type: String,
+    require: [true, "A user must have a email."],
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: [true, "A user must have a password."],
+  },
+  passwordConfirm: {
+    type: String,
+    require: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
