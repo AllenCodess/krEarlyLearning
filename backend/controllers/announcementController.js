cdc58e088637b59181b0ade0a8b4bac8284d61a7
AllenCodess <Aph296@protonmail.com>
2026-09-17 18:59:56 -0400

    successfullt created the view post api

--- backend/controllers/announcementController.js
+++ backend/controllers/announcementController.js
@@ -16,3 +16,12 @@ export const createPost = async (req, res) => {
     res.status(400).json({ status: "success", message: error.message });
   }
 };
+
+export const viewPost = async (req, res) => {
+  try {
+    const posts = await Posts.find();
+    res.status(200).json({ status: "success", data: posts });
+  } catch (error) {
+    res.status(400).json({ status: "failed", message: error.message });
+  }
+};
--- backend/routes/announcementRoute.js
+++ backend/routes/announcementRoute.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { admin, protect } from "../middleware/authMiddleware.js";
 
-import { createPost } from "../controllers/announcementController.js";
+import { createPost, viewPost } from "../controllers/announcementController.js";
 
 const router = express.Router();
 
-router.route("/").post(createPost);
+router.route("/").post(protect, admin, createPost).get(protect, viewPost);
 
 export default router;
