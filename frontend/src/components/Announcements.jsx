8c5c28b27459790799072f05bedbdd419d990626
AllenCodess <Aph296@protonmail.com>
2026-09-17 12:08:07 -0400

    started the structure of announcements

---
+++ frontend/src/components/Announcements.jsx
@@ -0,0 +1,20 @@
+export const Announcements = () => {
+  return (
+    <>
+      <div className="announcement-container">
+        <h1>Announcements</h1>
+        <div className="announcement-info-container">
+          <p className="announcement-severity">IMPORTANT NOTICE</p>
+          <h2 className="announcement-subject">School Closed Monday</h2>
+          <p className="announcement-date">September 14, 2026</p>
+          <hr />
+          <p className="announcement-desc">
+            KR Early Learning will be closed on Monday, September 14 for staff development day. We
+            will reopen on tuesday at our normal hours. If you have any questions please contact the
+            office.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
--- frontend/src/pages/ParentPortal.jsx
+++ frontend/src/pages/ParentPortal.jsx
@@ -1,3 +1,5 @@
+import { Announcements } from "../components/Announcements";
+
 export const ParentPortal = () => {
   return (
     <>
@@ -7,8 +9,24 @@ export const ParentPortal = () => {
           <p className="pp-desc">Stay informed, connected, and part of our learning community.</p>
         </div>
         <div className="parent-portal-content">
-          <div className="pp-content-left"></div>
-          <div className="pp-content-right"></div>
+          <div className="pp-content-left">
+            <h2>Dashboard</h2>
+            <p>
+              Quick access to announcements, documents, and important updates from KR Early
+              Learning.
+            </p>
+          </div>
+          <div className="pp-content-right">
+            <div className="pp-content-right-header">
+              <h1>Welcome back, Sarah</h1>
+              <p>Here's what's new at KR Early Learning.</p>
+            </div>
+            <div className="pp-content-right-btn">
+              <button className="pp-btn">Announcements</button>
+              <button className="pp-btn">Documents</button>
+            </div>
+            <Announcements />
+          </div>
         </div>
       </div>
     </>
