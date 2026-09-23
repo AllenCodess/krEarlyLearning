ece99fee57c0f658cadb479cdc9fff7b6ac8d9e6
AllenCodess <Aph296@protonmail.com>
2026-09-23 13:29:29 -0400

    successfully created the delete api

--- backend/controllers/documentController.js
+++ backend/controllers/documentController.js
@@ -34,3 +34,12 @@ export const viewDocuments = async (req, res) => {
     res.status(400).json({ status: "failed", message: error.message });
   }
 };
+
+export const deleteDocument = async (req, res) => {
+  try {
+    const doc = await Documents.findByIdAndDelete(req.params.id);
+    res.status(200).json({ status: "success", message: "Document has been deleted." });
+  } catch (error) {
+    res.status(400).json({ status: "fail", message: error.message });
+  }
+};
--- backend/routes/documentRoute.js
+++ backend/routes/documentRoute.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { admin, protect } from "../middleware/authMiddleware.js";
-import { createDocument, viewDocuments } from "../controllers/documentController.js";
+import {
+  createDocument,
+  viewDocuments,
+  deleteDocument,
+} from "../controllers/documentController.js";
 import upload from "../middleware/upload.js";
 
 const router = express.Router();
 
 router.route("/uploads").post(protect, admin, upload.single("file"), createDocument);
 router.route("/").get(viewDocuments);
+router.route("/:id").delete(protect, admin, deleteDocument);
 export default router;
